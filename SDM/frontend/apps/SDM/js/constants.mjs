c91d6603fbe723fadf0abce9f9939acb8e87c1a1
Sandeep Anagandula <sandeepanagandula007@gmail.com>
2021-02-03 14:52:30 +0530

    first commit

---
+++ SDM/frontend/apps/SDM/js/constants.mjs
@@ -0,0 +1,21 @@
+const FRONTEND = "http://localhost:8080";
+const BACKEND = "http://localhost:9090";
+const APP_NAME = "sample";
+const APP_PATH = `${FRONTEND}/apps/${APP_NAME}`;
+export const APP_CONSTANTS = {
+ FRONTEND, BACKEND, APP_PATH, APP_NAME,
+ MESSAGE_HTML: APP_PATH + "/message.html",
+ RANDOM_HTML: APP_PATH + "/random.html",
+ SESSION_NOTE_ID: "com_monkshu_ts",
+ USERID: "id",
+ USER_ROLE: "user",
+ GUEST_ROLE: "guest",
+ PERMISSIONS_MAP: {
+ user: [APP_PATH + "/message.html",
+$$.MONKSHU_CONSTANTS.ERROR_THTML],
+ guest: [APP_PATH + "/random.html", APP_PATH + "/message.html",
+$$.MONKSHU_CONSTANTS.ERROR_THTML]
+ },
+ API_KEYS: { "*": "uiTmv5YBOZMqdTb0gekD40PnoxtB9Q0k" },
+ KEY_HEADER: "X-API-Key"
+}
